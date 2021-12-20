af8b11f14324c5fdb00c8edccfdbf23b52f8ca72
Léo Araújo <leocraraujo@gmail.com>
2021-12-20 08:04:40 -0300

    Att Home

--- src/components/Servidor-pesquisa/index.tsx
+++ src/components/Servidor-pesquisa/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { api } from "../../services/api";
-import { Input, Table, Tag, Space, Modal, Select } from "antd";
+import { Input, Table, Tag, Space, Modal, Select, Button } from "antd";
 import { AudioOutlined } from "@ant-design/icons";
 const { Column, ColumnGroup } = Table;
 
@@ -31,7 +31,9 @@ type ServidorData = IServidorData;
 
 export function ServidorPesquisa() {
   const [editando, setEditando] = useState(false);
+  const [adicionando, setAdicionando] = useState(false);
   const [editServ, setEditServ] = useState<ServidorData>();
+  const [addServ, setAddServ] = useState<ServidorData>();
 
   const [lotacoes, setLotacoes] = useState<Lotacoes[]>([]);
 
@@ -43,11 +45,55 @@ export function ServidorPesquisa() {
 
   const [servidores, setServidores] = useState<Servidor[]>([]);
 
+  useEffect(() => {
+    api.get("/servidores").then((response) => {
+      setServidores(response.data);
+    });
+  }, []);
+
   const editarServidor = (record: any) => {
     console.log(record);
     setEditando(true);
     setEditServ({ ...record });
   };
+
+  const addServidor = () => {
+    setAdicionando(true);
+    // setEditServ({ ...record });
+  };
+
+  const adicionado = () => {
+    // setAddServ(form);
+    const data = addServ;
+    data.dateCreated = new Date();
+    data.dateUpdated = new Date();
+    // form.resetFields();
+
+    return ServidorDataService.create(data)
+      .then(() => {
+        console.log(data);
+        console.log(addServ);
+        api.get("/servidores").then((response) => {
+          setServidores(response.data);
+        });
+        // router.push("/servidor-list");
+      })
+      .catch(Error);
+  };
+
+  function atualizado() {
+    const data = editServ;
+    const id = editServ?.id;
+    data.dateUpdated = new Date();
+
+    return ServidorDataService.update(data, id)
+      .then(() => {
+        return setServidores([]);
+        // api.get("/servidores").then((response) => {
+        // });
+      })
+      .catch(Error);
+  }
 
   const onSearch = (matricula: any) => {
     ServidorDataService.findByTitle(matricula)
@@ -80,24 +126,15 @@ export function ServidorPesquisa() {
     });
   };
 
+  const resetaAdicionando = () => {
+    setAdicionando(false);
+    setAddServ(undefined);
+  };
+
   const resetaEditando = () => {
     setEditando(false);
     setEditServ(undefined);
   };
-
-  function atualizado() {
-    const data = editServ;
-    const id = editServ?.id;
-    data.dateUpdated = new Date();
-
-    return ServidorDataService.update(data, id)
-      .then(() => {
-        return setServidores([]);
-        // api.get("/servidores").then((response) => {
-        // });
-      })
-      .catch(Error);
-  }
 
   return (
     <>
@@ -108,7 +145,18 @@ export function ServidorPesquisa() {
           onSearch={onSearch}
           enterButton
         />
+        <Button
+          type="primary"
+          // =========================================
+          onClick={() => {
+            addServidor();
+          }}
+        >
+          Adicionar Servidor
+        </Button>
       </Space>
+      <h1>Lista de Servidores:</h1>
+
       <Table dataSource={servidores} className="table">
         <Column title="Nome" dataIndex="nome" key="nome" />
         <Column title="Matricula" dataIndex="matricula" key="matricula" />
@@ -183,6 +231,69 @@ export function ServidorPesquisa() {
           allowClear
           onChange={(e) => {
             setEditServ((pre: any) => {
+              return { ...pre, lotacao: e };
+            });
+          }}
+        >
+          {lotacoes.map((lotacao) => {
+            return (
+              <Option value={lotacao.id} key={lotacao.id}>
+                {" "}
+                {lotacao.descricao}
+              </Option>
+            );
+          })}
+        </Select>
+      </Modal>
+
+      <Modal
+        title="Adicionar Servidor"
+        visible={adicionando}
+        okText="Salvar"
+        cancelText="Cancelar"
+        onCancel={() => {
+          resetaAdicionando();
+        }}
+        onOk={() => {
+          console.log(addServ);
+          adicionado();
+          resetaAdicionando();
+        }}
+      >
+        <p>Nome</p>
+        <Input
+          value={editServ?.nome}
+          maxLength={400}
+          onChange={(e) => {
+            setAddServ((pre: any) => {
+              return { ...pre, nome: e.target.value };
+            });
+            console.log(addServ);
+          }}
+        />
+        <br />
+        <br />
+
+        <p>Matricula</p>
+        <Input
+          value={editServ?.matricula}
+          maxLength={30}
+          onChange={(e) => {
+            setAddServ((pre: any) => {
+              return { ...pre, matricula: e.target.value };
+            });
+          }}
+        />
+        <br />
+        <br />
+
+        <p>Lotação</p>
+        <Select
+          placeholder="Selecione uma opção de lotação."
+          // onChange={}
+          allowClear
+          onChange={(e) => {
+            setAddServ((pre: any) => {
               return { ...pre, lotacao: e };
             });
           }}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { Layout, Menu } from "antd";
 
 import { ServidorLista } from "../components/Servidor-list";
 import { AddSider } from "../components/Sider";
+import { ServidorPesquisa } from "../components/Servidor-pesquisa/index";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -35,15 +36,11 @@ const Home: NextPage = () => {
           <Nav />
         </Header>
         <Content style={{ margin: "0 16px" }}>
-          {/* <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb> */}
           <div
             className="site-layout-background"
             style={{ padding: 24, minHeight: 360 }}
           >
-            <ServidorLista />
+            <ServidorPesquisa />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
